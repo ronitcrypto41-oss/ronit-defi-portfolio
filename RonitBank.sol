c221d3b17b7483eb9976383fbec6ce15015e5b9a
ronit-lending-protocol <ronitcrypto41@gmail.com>
2026-06-03 06:44:00 +0530

    Update RonitBank.sol

--- RonitBank.sol
+++ RonitBank.sol
@@ -1,28 +1,35 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-contract RonitBank {
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v5.0.0/contracts/access/Ownable.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v5.0.0/contracts/utils/ReentrancyGuard.sol";
+
+contract RonitBank is Ownable, ReentrancyGuard {
+
     mapping(address => uint256) public balances;
     uint256 public totalDeposited;
-    address public owner;
 
-    constructor() {
-        owner = msg.sender;
-    }
+    event Deposit(address indexed user, uint256 amount);
+    event Withdraw(address indexed user, uint256 amount);
+    event Transfer(address indexed from, address indexed to, uint256 amount);
+
+    constructor() Ownable(msg.sender) {}
 
     function deposit() public payable {
         require(msg.value > 0, "Cannot deposit 0 ETH");
         balances[msg.sender] += msg.value;
         totalDeposited += msg.value;
+        emit Deposit(msg.sender, msg.value);
     }
 
-    function withdraw(uint256 amount) public {
+    function withdraw(uint256 amount) public nonReentrant {
         require(amount > 0, "Cannot withdraw 0 ETH");
         require(balances[msg.sender] >= amount, "Insufficient balance");
         balances[msg.sender] -= amount;
         totalDeposited -= amount;
         (bool success, ) = payable(msg.sender).call{value: amount}("");
         require(success, "ETH transfer failed");
+        emit Withdraw(msg.sender, amount);
     }
 
     function transfer(address to, uint256 amount) public {
@@ -30,26 +37,22 @@ contract RonitBank {
         require(to != address(0), "Cannot transfer to zero address");
         balances[msg.sender] -= amount;
         balances[to] += amount;
+        emit Transfer(msg.sender, to, amount);
     }
 
-    function emergencyWithdraw() public {
-        require(msg.sender == owner, "Only owner can withdraw");
+    function emergencyWithdraw() public onlyOwner {
         uint256 contractBalance = address(this).balance;
-        require(contractBalance > 0, "No funds to withdraw");
-        (bool success, ) = payable(msg.sender).call{value: contractBalance}("");
+        require(contractBalance > 0, "Nothing to withdraw");
+        (bool success, ) = payable(owner()).call{value: contractBalance}("");
         require(success, "Emergency withdraw failed");
+    }
+
+    function getBalance() public view returns (uint256) {
+        return balances[msg.sender];
     }
 
     function getContractBalance() public view returns (uint256) {
         return address(this).balance;
     }
-
-    function getUserInfo(address user) public view returns (uint256 balance, uint256 contractTotal) {
-        return (balances[user], address(this).balance);
-    }
-
-    function getBalance(address user) public view returns (uint256) {
-        return balances[user];
-    }
 }
-
+      
